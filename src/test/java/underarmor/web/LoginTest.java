24a81c3ba7814ef96374fc972cba701d8ed7266a
Yevhen Levchenko <yevchenko@solvd.com>
2023-09-14 12:33:53 +0300

    Changed Login Test

--- src/test/java/underarmor/web/LoginTest.java
+++ src/test/java/underarmor/web/LoginTest.java
@@ -27,27 +27,4 @@ public class LoginTest extends BaseTest {
 
     }
 
-    @Test
-    void shouldClickButton() {
-        page.navigate("data:text/html,<script>var result;</script><button onclick='result=\"Clicked\"'>Go</button>");
-        page.locator("button").click();
-        assertEquals("Clicked", page.evaluate("result"));
-    }
-
-    @Test
-    void shouldCheckTheBox() {
-        page.setContent("<input id='checkbox' type='checkbox'></input>");
-        page.locator("input").check();
-        assertTrue((Boolean) page.evaluate("() => window['checkbox'].checked"));
-    }
-
-    @Test
-    void shouldSearchWiki() {
-        page.navigate("https://www.wikipedia.org/");
-        page.locator("input[name=\"search\"]").click();
-        page.locator("input[name=\"search\"]").fill("playwright");
-        page.locator("input[name=\"search\"]").press("Enter");
-        assertEquals("https://en.wikipedia.org/wiki/Playwright", page.url());
-    }
-
 }
